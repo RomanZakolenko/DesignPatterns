a7bcbcbb5e0d5d1d59ddd46353f45d7bc5c200d6
Roman Zakolenko <zakolenkoroman@gmail.com>
2015-08-07 20:35:16 +0300

    mediator

---
+++ src/behavioral/mediator/BodyPart.java
@@ -0,0 +1,13 @@
+package behavioral.mediator;
+
+public class BodyPart {
+	private final Brain brain;
+	
+	public BodyPart(Brain brain){
+		this.brain = brain;
+	}
+	
+	public void action(){
+		brain.listenAction();
+	}
+}
---
+++ src/behavioral/mediator/Ear.java
@@ -0,0 +1,14 @@
+package behavioral.mediator;
+
+public class Ear extends BodyPart {
+	private String sound;
+
+	public Ear(Brain brain) {
+		super(brain);
+	}
+
+	public void hear(String sound) {
+		this.sound = sound;
+		action();
+	}
+}
